f55f483fc11ff3da0b37cd55df590b82d6c2a6af
andresshare <andresshare@gmail.com>
2017-09-16 23:48:57 -0500

    update migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917010757) do
+ActiveRecord::Schema.define(version: 20170917044116) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "color"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
